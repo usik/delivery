fc24ff78dfd5bcd1b3b9a4733c34aeb884a4d445
yusik Kim <usik.o2@gmail.com>
2021-04-12 18:24:25 +0900

    adding menu

--- models/Shops.js
+++ models/Shops.js
@@ -13,6 +13,19 @@ module.exports = (sequelize, DataTypes) => {
         }
     );
 
+    //menu model relation
+    Shops.associate = (models) => {
+        //메뉴 모델에 외부키를 건다
+        //onDelete 옵션의 경우 제품 하나가 삭제되면 외부키가 걸린 메모들도 싹다 삭제하기
+
+        Shops.hasMany(models.ShopsMenu, {
+            as: 'Menu',
+            foreignKey: 'shop_id',
+            sourceKey: 'id',
+            onDelete: 'CASCADE'
+        });
+    }
+
     Shops.prototype.dateFormat = (date) => (
         moment(date).format('YYYY-MM-DD')
     );
