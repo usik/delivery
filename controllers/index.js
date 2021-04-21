e55f62307be53897169f5ef5e6ceccaa9982c4e3
yusik Kim <usik.o2@gmail.com>
2021-04-21 09:44:47 +0900

    adding landing page

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,5 @@ const router = Router()
 router.use('/admin', require('./admin'));
 router.use('/accounts', require('./accounts'));
 router.use('/auth', require('./auth'));
+router.use('/', require('./home'));
 module.exports = router;
