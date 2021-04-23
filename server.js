4769f79150e8afae860763a780dc00223a3f2a1d
yusik Kim <usik.o2@gmail.com>
2021-04-23 17:47:14 +0900

    adding server side chat

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const port = 3000;
 app.listen( port, function(){
     console.log('Express listening on port', port);
 });
+
+//server side socket io
+const listen = require('socket.io');
+const io = listen(server);
+io.on('connection', (socket)=>{
+    socket.on('client message', (data)=>{
+        io.emit('server message', data.message);
+    });
+});
