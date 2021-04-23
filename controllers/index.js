7a807f173137515294fa398f046b71a5d9fa66c4
yusik Kim <usik.o2@gmail.com>
2021-04-23 17:09:56 +0900

    adding chat

--- controllers/index.js
+++ controllers/index.js
@@ -4,5 +4,6 @@ const router = Router()
 router.use('/admin', require('./admin'));
 router.use('/accounts', require('./accounts'));
 router.use('/auth', require('./auth'));
+router.use('/chat', require('./chat'));
 router.use('/', require('./home'));
 module.exports = router;
