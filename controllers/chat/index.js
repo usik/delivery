0ad05fecbda1a4156ae017801a62ee47315acaaa
Chaesoo Rim <chaesoo.rim@navercorp.com>
2021-04-24 10:00:21 +0900

    chat service after login

--- controllers/chat/index.js
+++ controllers/chat/index.js
@@ -1,8 +1,14 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/', (_, res) => {
-    res.render('chat/index.html');
+router.get('/', (req, res) => {
+    if(!req.isAuthenticated()){
+        res.send('<script>alert("로그인이 필요한 서비스입니다.");\
+        location.href="/accounts/login";</script>');
+    }else{
+        res.render('chat/index.html');    
+    }
+    
 });
 
 module.exports=router;
